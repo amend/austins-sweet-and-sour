cc0949efbb6e81b5c560499dd7f949113e5284ea
Andoni Mendoza <andoni.mendoza@gmail.com>
2017-07-08 21:22:08 -0700

    small syntax fix

--- config.js
+++ config.js
@@ -3,12 +3,3 @@ exports.mongodb = {
   uri: 'mongodb://localhost/tweets/make_austintweets_sentiment_collection',
   auto_reconnect: true
 };
-
-
-
-/*
-exports.mongodb = {
-  uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/reQwip',
-  auto_reconnect: true
-};
-*/
